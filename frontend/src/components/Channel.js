671663bf2eb6157437ea6ed47853cfb1d0456609
nicksome168 <b05502058@ntu.edu.tw>
2021-06-12 17:22:01 +0800

    add data interface modifer in channel component

--- frontend/src/components/Channel.js
+++ frontend/src/components/Channel.js
@@ -17,6 +17,10 @@ function Channel(props) {
   var { channel_id } = props || '2';
   var [messageList, setMessageList] = useState([]);
   var [inputRef, setInputRef] = useState(React.createRef());
+  // const currentUser = AuthService.getCurrentUser();
+  const currentUser = { username: 'Nick', user_id: 'b05502058' };
+  const currentUserName = currentUser ? currentUser.username : 'NotFoundName';
+
   const sender_id = AuthService.getCurrentUser() || '1';
   const sender_avatar = '';
 
@@ -46,46 +50,85 @@ function Channel(props) {
     setInputRef(inputRef);
   };
 
+  const preprocess = (msgs) => {
+    return msgs.map((msg) => {
+      return {
+        title:
+          msg.sender_id === currentUser.user_id ? currentUserName : 'Others',
+        position: msg.sender_id === currentUser.user_id ? 'right' : 'left',
+        type: msg.type,
+        text: msg.text,
+        data: { uri: msg.data },
+        date: msg.datetime,
+        avatar: msg.sender_avatar,
+        dateString: msg.datetime.toLocaleString().split(',')[1],
+      };
+    });
+  };
+
   const getMessageList = async () => {
-    let data = await MsgService.getAllMsgs(channel_id);
-    setMessageList([
+    // let data = await MsgService.getAllMsgs(channel_id);
+    let get_data = [
       {
-        title: 'john',
-        position: 'right',
+        message_id: '',
         type: 'text',
         text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-        date: new Date(),
-        avatar: 'https://randomuser.me/api/portraits/men/60.jpg',
-        dateString: new Date().toLocaleString().split(',')[1],
+        image_url: '',
+        sender_id: 'b05502058',
+        sender_avatar: 'https://randomuser.me/api/portraits/men/60.jpg',
+        datetime: new Date(),
       },
       {
-        title: 'john',
-        position: 'left',
-        type: 'text',
+        message_id: '',
+        type: 'photo',
         text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-        date: new Date(),
-        avatar: 'https://randomuser.me/api/portraits/men/67.jpg',
-        dateString: new Date().toLocaleString().split(',')[1],
+        image_url: '',
+        sender_id: 'b05502058',
+        sender_avatar: 'https://randomuser.me/api/portraits/men/60.jpg',
+        data: 'https://randomuser.me/api/portraits/men/60.jpg',
+        datetime: new Date(),
       },
-      {
-        title: 'john',
-        position: 'right',
-        type: 'text',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-        date: new Date(),
-        avatar: 'https://randomuser.me/api/portraits/women/67.jpg',
-        dateString: new Date().toLocaleString().split(',')[1],
-      },
-      {
-        title: 'john',
-        position: 'right',
-        type: 'text',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-        date: new Date(),
-        avatar: 'https://randomuser.me/api/portraits/women/67.jpg',
-        dateString: new Date().toLocaleString().split(',')[1],
-      },
-    ]);
+    ];
+    const data = preprocess(get_data);
+    // let data = [
+    //   {
+    //     title: 'john',
+    //     position: 'right',
+    //     type: 'text',
+    //     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    //     date: new Date(),
+    //     avatar: 'https://randomuser.me/api/portraits/men/60.jpg',
+    //     dateString: new Date().toLocaleString().split(',')[1],
+    //   },
+    //   {
+    //     title: 'john',
+    //     position: 'left',
+    //     type: 'text',
+    //     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    //     date: new Date(),
+    //     avatar: 'https://randomuser.me/api/portraits/men/67.jpg',
+    //     dateString: new Date().toLocaleString().split(',')[1],
+    //   },
+    //   {
+    //     title: 'john',
+    //     position: 'right',
+    //     type: 'text',
+    //     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    //     date: new Date(),
+    //     avatar: 'https://randomuser.me/api/portraits/women/67.jpg',
+    //     dateString: new Date().toLocaleString().split(',')[1],
+    //   },
+    //   {
+    //     title: 'john',
+    //     position: 'right',
+    //     type: 'text',
+    //     text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    //     date: new Date(),
+    //     avatar: 'https://randomuser.me/api/portraits/women/67.jpg',
+    //     dateString: new Date().toLocaleString().split(',')[1],
+    //   },
+    // ];
+    setMessageList(data);
   };
 
   const onChange = async (e) => {
