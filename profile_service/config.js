b7adfc023e98b57ea6191e31bdd0398e1627f4c3
lewiechiu <louiechiu@gmail.com>
2021-06-13 16:17:09 +0800

    Added config file for docker

---
+++ profile_service/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ENV_NAME: process.env.ENV_NAME,
+  
+    PORT: process.env.PORT || 8080,
+    REGION: process.env.REGION || 'us-east-1',
+  
+    REDIS_ENDPOINT: process.env.REDIS_ENDPOINT || 'localhost',
+  };
+  
+
