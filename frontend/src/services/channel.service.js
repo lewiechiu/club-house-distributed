045e8d01c5aaa600ac5fc8879aa8d3b6567f8190
nicksome168 <b05502058@ntu.edu.tw>
2021-06-16 22:41:57 +0800

    fixed channel.service channelList bugs

--- frontend/src/services/channel.service.js
+++ frontend/src/services/channel.service.js
@@ -30,71 +30,72 @@ const channelListData = [
     // },
 ];
 
-
 const useChannel = () => {
     const [channelList, setChannelList] = useState(channelListData);
     const currentUser = AuthService.getCurrentUser();
 
     const reloadList = (data) => {
-        let idx = channelList.findIndex(x => x.channel_id == data.channel_id);
-        if (idx == -1){
-            setChannelList([...channelList, data])
-        }else{
+        let idx = channelList.findIndex(
+            (x) => x.channel_id === data.channel_id
+        );
+        if (idx === -1) {
+            setChannelList([...channelList, data]);
+        } else {
             setChannelList([
                 ...channelList.slice(0, idx),
                 data,
-                ...channelList.slice(idx+1),
-            ])
+                ...channelList.slice(idx + 1),
+            ]);
         }
         //resort
         // let newList = channelList
         // let newSortList = newList.sort((a, b) => (a.people_count > b.people_count ? 1 : -1));
         // setChannelList(newSortList.slice(0, -1));
-    }
+    };
 
     useEffect(() => {
         socket.on('receive_channel', (response) => {
             const { action, data } = response;
-            switch(action) {
-                case 'enter': 
+            switch (action) {
+                case 'enter':
                     console.log(`Sombody enter channel ${data.channel_name}.`);
                     // reloadList(data);
                     break;
-                case 'leave': 
+                case 'leave':
                     console.log(`Sombody leave channel ${data.channel_name}.`);
                     break;
-                case 'create': 
+                case 'create':
                     console.log(`Sombody create channel ${data.channel_name}.`);
                     break;
             }
-            console.log("res", response);
+            console.log('res', response);
         });
     });
 
-    const getAllChannels = async(channel_id) => {
-        if (!socket) return("Scoket connect fail!!!");
+    const getAllChannels = async (channel_id) => {
+        if (!socket) return 'Scoket connect fail!!!';
 
-        let data = {
+        let payload = {
             username: currentUser.username,
             token: currentUser.token,
             action: 'get_all',
             channel_id: channel_id,
         };
-    
+
         try {
-            socket.emit('channel', data);
-            socket.on('channel_response', (response) => {
-                if (Array.isArray(response)){ //should be "action" === "get_all"
-                    setChannelList(response)
-                    // console.log(channelList);
-                }
+            socket.emit('channel', payload);
+            let { action, data } = await new Promise((resolve) => {
+                socket.on('channel_response', (response) => {
+                    resolve(response);
+                });
             });
+            if (action === 'get_all') setChannelList(data);
         } catch (err) {
             console.error(err);
         }
-    }
+    };
 
-    const createChannel = async(channel_name, cb) => {
+    const createChannel = async (channel_name, cb) => {
         let data = {
             username: currentUser.username,
             token: currentUser.token,
@@ -107,67 +108,87 @@ const useChannel = () => {
             socket.emit('channel', data);
             // console.log(`[Create_send]User ${currentUser.username} create channel channel_name(${channel_id})!`)
             socket.on('channel_response', (response) => {
-                if(response.constructor === Object && "channel_id" in response){ //should be "action" === "create"
-                    const {message, channel_id} = response;
+                if (
+                    response.constructor === Object &&
+                    'channel_id' in response
+                ) {
+                    const { action, message, channel_id } = response;
                     // console.log(`[Create_res]User ${currentUser.username} create channel channel_name(${channel_id})!`)
-                    const newChannelData = {
-                        channel_id: channel_id,
-                        channel_name: channel_name,
-                        people_count: 1,
-                        users: [{username: currentUser.username, avatar_url: ""}]
+                    if ((action === 'create') & (message === 'Success')) {
+                        const newChannelData = {
+                            channel_id: channel_id,
+                            channel_name: channel_name,
+                            people_count: 1,
+                            users: [
+                                {
+                                    username: currentUser.username,
+                                    avatar_url: '',
+                                },
+                            ],
+                        };
+                        setChannelList([...channelList, newChannelData]);
+                        return cb(null, channel_id);
                     }
-                    setChannelList([...channelList, newChannelData]);
-                    return cb(null, channel_id);
                 }
             });
         } catch (err) {
             console.error(err);
             return cb(err, null);
         }
-        
-    }
+    };
 
-    const leaveChannel = async(channel_id) => {
+    const leaveChannel = async (channel_id) => {
         let data = {
             username: currentUser.username,
             token: currentUser.token,
             action: 'leave',
-            channel_id: channel_id
+            channel_id: channel_id,
         };
 
         try {
             socket.emit('channel', data);
-            // console.log(`[Leave_send]User ${currentUser.username} leave channel ${channel_id}!`)
+            console.log(
+                `[Leave_send]User ${currentUser.username} leave channel ${channel_id}!`
+            );
             socket.on('channel_response', (response) => {
-                if(response.constructor === Object && Object.keys(response).length===1){ //should be "action" === "leave"
-                    const {message} = response;
-                    // if (message === "Success"){
-                        // console.log(`[Leave_res]User ${currentUser.username} leave channel ${channel_id}!`)
-                        //// update channelList state(user_cnt-1)
-                        // let idx = channelList.findIndex(x => x.channel_id == channel_id);
-                        // if(idx !== -1){
-                        //     let updateChannelData = channelList[idx];
-                        //     updateChannelData.people_count = parseInt(updateChannelData.people_count) - 1;
-                        //     if(updateChannelData.people_count == 0){
-                        //         setChannelList([
-                        //             ...channelList.slice(0, idx),
-                        //             ...channelList.slice(idx+1),
-                        //         ])
-                        //     }else{
-                        //         setChannelList([
-                        //             ...channelList.slice(0, idx),
-                        //             updateChannelData,
-                        //             ...channelList.slice(idx+1),
-                        //         ])
-                        //     }
-                        // } 
-                    // }
+                console.log(response);
+                if (
+                    response.constructor === Object &&
+                    Object.keys(response).length === 1
+                ) {
+                    const { action, message } = response;
+                    if ((action === 'leave') & (message === 'Success')) {
+                        console.log(
+                            `[Leave_res]User ${currentUser.username} leave channel ${channel_id}!`
+                        );
+                        // update channelList state(user_cnt-1)
+                        let idx = channelList.findIndex(
+                            (x) => x.channel_id === channel_id
+                        );
+                        if (idx !== -1) {
+                            let updateChannelData = channelList[idx];
+                            updateChannelData.people_count =
+                                parseInt(updateChannelData.people_count) - 1;
+                            if (updateChannelData.people_count === 0) {
+                                setChannelList([
+                                    ...channelList.slice(0, idx),
+                                    ...channelList.slice(idx + 1),
+                                ]);
+                            } else {
+                                setChannelList([
+                                    ...channelList.slice(0, idx),
+                                    updateChannelData,
+                                    ...channelList.slice(idx + 1),
+                                ]);
+                            }
+                        }
+                    }
                 }
             });
         } catch (err) {
             console.error(err);
         }
-    }
+    };
 
     const enterChannel = (channel_id) => {
         let data = {
@@ -175,33 +196,43 @@ const useChannel = () => {
             token: currentUser.token,
             channel_id: channel_id,
             action: 'enter',
-            avatar_url: ""
+            avatar_url: '',
         };
 
         try {
             socket.emit('channel', data);
-            // console.log(`[Enter_send]User ${currentUser.username} enter channel ${channel_id}!`)
+            console.log(
+                `[Enter_send]User ${currentUser.username} enter channel ${channel_id}!`
+            );
             socket.on('channel_response', (response) => {
-                if(response.constructor === Object && "user_count" in response){ //should be "action" === "enter"
-                    const {people_count, message} = response;
-                    // if (message === "Success"){
-                        // console.log(`[Enter_res]User ${currentUser.username} enter channel ${channel_id}!`)
-                        // // update channelList state(user_cnt+1)
-                        // let idx = channelList.findIndex(x => x.channel_id == channel_id);
-                        // let updateChannelData = channelList[idx];
-                        // updateChannelData.people_count = parseInt(updateChannelData.people_count) + 1;
-                        // setChannelList([
-                        //     ...channelList.slice(0, idx),
-                        //     updateChannelData,
-                        //     ...channelList.slice(idx+1),
-                        // ])
-                    // }
+                if (
+                    response.constructor === Object &&
+                    'user_count' in response
+                ) {
+                    const { action, user_count, message } = response;
+                    if ((action === 'enter') & (message === 'Success')) {
+                        console.log(
+                            `[Enter_res]User ${currentUser.username} enter channel ${channel_id}!`
+                        );
+                        // update channelList state(user_cnt+1)
+                        let idx = channelList.findIndex(
+                            (x) => x.channel_id === channel_id
+                        );
+                        let updateChannelData = channelList[idx];
+                        updateChannelData.people_count =
+                            parseInt(updateChannelData.people_count) + 1;
+                        setChannelList([
+                            ...channelList.slice(0, idx),
+                            updateChannelData,
+                            ...channelList.slice(idx + 1),
+                        ]);
+                    }
                 }
             });
         } catch (err) {
             console.error(err);
         }
-    }
+    };
 
     return {
         channelList,
@@ -210,6 +241,6 @@ const useChannel = () => {
         enterChannel,
         leaveChannel,
     };
-}
+};
 
 export default useChannel;
