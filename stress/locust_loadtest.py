171d1edd83938a544d6178eaf2f57003a8e330ed
yuhsuanlin23130 <64848202+yuhsuanlin23130@users.noreply.github.com>
2021-06-17 01:46:58 +0800

    update locust_loadtest.py

--- stress/locust_loadtest.py
+++ stress/locust_loadtest.py
@@ -1,7 +1,9 @@
+# locust -f locust_loadtest.py -H http://localhost:8888
 from locust import HttpUser, task, between
 from random import randint
 from datetime import datetime
-
+import socketio  #https://python-socketio.readthedocs.io/en/latest/client.html
+ 
 def makename(length):
     result = ''
     characters = 'abcdefghijklmnopqrstuvwxyz_ '
@@ -15,34 +17,61 @@ class QuickstartUser(HttpUser):
     wait_time = between(0.5, 1)
     
     def on_start(self):  
-        self.user_id = self.login()
+        self.sio = socketio.Client()
+        self.sio.connect("http://18.116.51.105:8080", transports='websocket')
+        
+        self.username, self.token, self.channel_id = "", "", ""
+        self.login()
+        
+        '''@self.sio.on('login_response')
+        def login_response(data):
+            data = data.json()
+            self.username = data['username']
+            self.token = data['token']
+            
+        
+        @self.sio.on('channel_response')
+        def channel_response(data):
+            data = data.json()
+            if data["action"] == 'get_all':
+                res_list = data["data"]
+                if len(res_list) != 0:
+                    idx = randint(0,len(res_list))
+                    self.channel_id = res_list[idx]["channel_id"]  
+            elif data["action"] == 'create':
+                self.channel_id = data["channel_id"]
+            
+        '''
+        
     
     @task
     def loadtest(self):  
+        
         while True:
             self.channel_id = self.all_channels()  # one channel_id is selected
             if randint(1,8)==1:
                 res = self.channel_create()  
                 if res != '':
                     self.channel_id = res
-                    print(self.user_id, 'create', self.channel_id)
+                    self.get_messages()   
+                    #print(self.username, 'create', self.channel_id)
             else:
                 self.channel_enter()   
-                print(self.user_id, 'enter', self.channel_id)
+                self.get_messages()   
+                #print(self.username, 'enter', self.channel_id)
 
-            #self.get_messages()       ### TODO
             while randint(1,10)!=1:
-                #self.send_messages()  ### TODO
-                #self.get_messages()   ### TODO
+                self.send_messages() 
             
-            print(self.user_id, 'leave', self.channel_id)
-            res = self.channel_leave()  
+            #print(self.username, 'leave', self.channel_id)
+            self.channel_leave()  
 
             if randint(1,5)==1:
                 break
 
+
     def on_stop(self):
-        None
+        self.sio.disconnect()
 
     def hello_world(self):
         self.client.get("/")
@@ -51,38 +80,71 @@ class QuickstartUser(HttpUser):
         # create a user
         username =  makename(randint(5,14))
         password =  makename(randint(8,16))
-        response = self.client.post("/login", json={"username":username, "password":password}).json()
-        return username  ### 暫不處理error狀況
+        self.sio.emit('login', { "username":username, "password":password} )
         
+    
     def all_channels(self):
-        ### TODO 
-        # response = self.client.get("/all_channels", json={"limit": 100}).json()
-        # if len(response) != 0:
-        #    idx = randint(0,len(response))
-        #    return response[idx]["channel_id"]  
-        # return ''
-        channel_list = ['3SIOUyA1Kngjhs68', 'test2','CgxKh6xkXUCndMMV','M2x1Uucyg78km29B', 'K2nQfuSVzesLzkD7']
-        return channel_list[randint(0,4)]
-
+        data = {
+            "username": self.username,
+            "token": self.token,
+            "action": 'get_all',
+            "channel_id": self.channel_id
+        }
+    
+        self.sio.emit('channel', data)
+        
+            
     def channel_create(self):
-        response = self.client.post("/channel", json={"action": "create", "uid": self.user_id,"channel_name": makename(randint(8,20) )}).json()
-        try:
-            return response["channel_id"]
-        except:
-            return ''
+        data = {
+            "username": self.username,
+            "token": self.token,
+            "action": 'create',
+            "channel_name": makename(randint(8,20)),
+            "avatar_url": ""
+        }
+
+        self.sio.emit('channel', data)
         
     def channel_enter(self):
-        response = self.client.post("/channel", json={"action": "enter","channel_id": self.channel_id,"uid": self.user_id}).json()
+        data = {
+            "username": self.username,
+            "token": self.token,
+            "channel_id": self.channel_id,
+            "action": 'enter',
+            "avatar_url": ""
+        }
+        self.sio.emit('channel', data)
         
     def channel_leave(self):
-        response = self.client.post("/channel", json={"action": "leave","channel_id":self.channel_id,"uid": self.user_id}).json()
-            
+        data = {
+            "username": self.username,
+            "token": self.token,
+            "action": 'leave',
+            "channel_id": self.channel_id
+        }
+        self.sio.emit('channel', data)
+        
     def send_messages(self):
-        self.client.post("/message", json={"channel_id": self.channel_id,"type":"text", "text": makename(randint(3,20)), "sender_id": self.user_id,"sender_avatar": '',"datetime": datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+        data = {
+            "username": self.username,
+            "token": self.token,
+            "action": 'send',
+            "channel_id": self.channel_id,
+            "type": "type",
+            "text": makename(randint(3,20)),
+            "image_url": "",
+            "sender_avatar": "",
+            "datetime": ""
+        }
+        self.sio.emit('message', data)
         
     def get_messages(self, count = 30):
-        self.client.post("/all_messages", json={ "channel_id": self.channel_id, "message_id": '', "count": count})
+        data = {
+            "username": self.username,
+            "token": self.token,
+            "action": 'get_all',
+            "channel_id": self.channel_id,
+            "message_id": ""
+        }
+        self.sio.emit('message', data)
         
-    #def channel_reload(self):
-
-
